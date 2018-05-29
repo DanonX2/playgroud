ef535ada02894c22a846438d8faee80e281393a1
samuel <chensrj2@gmail.com>
2018-05-29 17:44:35 -0500

    ajdiof

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import pygame
+from pygame.locals import *
+import random
 BLACK = (   0,   0,   0)
 WHITE = ( 255, 255, 255)
 GREEN = (   0, 255,   0)
@@ -7,22 +9,36 @@ BLUE  = (   0,   0, 255)
 
 size =  (   1500,  1000)
 running = True
+x = size[0]/2
+y = size[1]/2
+'''
+def touching(obj1,obj2):
+    if obj1.x >= obj2.x '''
+class food:
+    def __init__(self):
+        self.x = random.randint(0,(size[0]-100))
+        self.y = random.randint(0,(size[1]-100))
+        self.Length = 100
+        self.weith = 100
+    def generate(self):
+        pygame.draw.rect(screen, RED, [self.x, self.y, self.Length, self.weith])
+class organism:
+    pass
+food1 = food()
+def main():
+    food1.generate()
 pygame.init()
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Playground")
 clock = pygame.time.Clock()
 while running:
+    keys = pygame.key.get_pressed()
     pygame.mouse.set_visible(1)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            print("User pressed a key.")
-        elif event.type == pygame.KEYUP:
-            print("User let go of a key.")
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            print("User pressed a mouse button")
     screen.fill(WHITE)
+    main()
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(144)
 pygame.quit()
