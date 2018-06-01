97b11ea7c193dbbae67c6ce78da39f6d01903e84
samuel <chensrj2@gmail.com>
2018-06-01 14:02:40 -0500

    32

--- NN.py
+++ NN.py
@@ -2,8 +2,10 @@ class neuron:
     def __init__(self,input,output):
         self.weight = 0
         self.bais = 0
+        self.inputl = input
+        self.outputl = output
         self.output = 0
-    def forward():
-        for i in input:
+    def forward(self):
+        for i in self.inputl:
             self.output = self.output + i*self.weight + self.bais
         return self.output
